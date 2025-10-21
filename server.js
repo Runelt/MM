256faa6a0afb4d6ea6796dcad532ddf9d80fcdae
Runelt <106849784+Runelt@users.noreply.github.com>
2025-10-21 16:44:29 +0900

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,20 @@ const PORT = process.env.PORT || 3000;
 // html 진입
 app.use(express.static('public'));
 
+// 확장자(.html) 제거
+app.get('/:page', (req, res, next) => {
+  const page = req.params.page;
+  const filePath = `${__dirname}/public/${page}.html`;
+  res.sendFile(filePath, (err) => {
+    if (err) next();
+  });
+});
+
+// 루트 페이지
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
 // 서버 구동
 app.listen(PORT, () => {
   console.log(`✅ http://localhost:${PORT}`);
